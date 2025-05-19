d45b058f69a7773feb4fa43bf433e678c8a737fd
Avadhut Jagde <avadhutjagde555@gmail.com>
2025-05-19 19:48:27 +0530

    [Course: 2. React 18 for Intermediate Topics > 2. Fetching and Updating Data with React Query (3h) ] [ Video: #7-Creating-a-Custom-Query-Hook_mp4_2min_32sec ] - create 'separation of concerns' using Custom-Query-Hook.

--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -1,26 +1,7 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-
-interface Todo {
-  id: number;
-  title: string;
-  userId: number;
-  completed: boolean;
-}
+import useTodos from "./hooks/useTodos";
 
 const TodoList = () => {
-  const fetchTodos = () =>
-    axios
-      .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
-      .then((res) => res.data);
-
-  // So at Run time, React query will call this "fetchTodos" function. When the promise is resolved, we get an array of to dos, then that array is stored in the "cache" against this "todos" key, okay?
-  // Now this "query" object has a bunch of properties like data, error is loading and so on
-  const { data: todos, error, isLoading } = useQuery<Todo[], Error>({
-    queryKey: ["todos"],
-    queryFn: fetchTodos,
-  });
+  const { data: todos, error, isLoading } = useTodos();
 
   if (isLoading) return <p>Loading...</p>;
 
